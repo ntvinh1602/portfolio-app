9267d3909fb3c7f1b47424639ede48883d7d308c
Vinh Nguyen <ntvinh1602@gmail.com>
2025-06-21 17:24:34 +0800

    chore: Squashed database migrations

--- supabase/migrations/0004_change_transaction_date_type.sql
+++
@@ -1,7 +0,0 @@
--- 0004_change_transaction_date_type.sql
--- This script changes the data type of the transaction_date column
--- in the transactions table from TIMESTAMPTZ to DATE.
-
-ALTER TABLE public.transactions
-ALTER COLUMN transaction_date TYPE DATE
-USING (transaction_date::date);
--- supabase/migrations/0008_add_borrow_transaction_type.sql
+++
@@ -1,5 +0,0 @@
--- Add 'borrow' to the transaction_type enum
--- This new type will be used to track transactions where money is borrowed,
--- creating a corresponding entry in the 'debts' table.
-
-ALTER TYPE public.transaction_type ADD VALUE 'borrow';
