cf1ce5247d83992771bcb42ffb43d6d78e6da6fc
Vinh Nguyen <ntvinh1602@gmail.com>
2025-07-11 00:53:07 +0800

    feat: Alter quantity columns in lot_consumptions and tax_lots tables to use numeric(14,8) type for improved precision

---
+++ supabase/migrations/20250710151918_alter_lot_consumptions_quantity_consumed.sql
@@ -0,0 +1,2 @@
+ALTER TABLE lot_consumptions
+ALTER COLUMN quantity_consumed TYPE numeric(14,8);
---
+++ supabase/migrations/20250710152435_alter_tax_lots_quantity_columns.sql
@@ -0,0 +1,3 @@
+ALTER TABLE tax_lots
+ALTER COLUMN original_quantity TYPE numeric(14,8),
+ALTER COLUMN remaining_quantity TYPE numeric(14,8);
